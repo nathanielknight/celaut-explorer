17a8453c108bef44945a654504e815c63cf5fed0
Nat Knight <nathaniel.ep@gmail.com>
2019-10-21 23:19:09 -0700

    New version of bound typescript file

--- ts_src/celaut_explorer.d.ts
+++ ts_src/celaut_explorer.d.ts
@@ -1,9 +1,28 @@
 /* tslint:disable */
+/**
+* Exposed struct for driving the simulation from JavaScript
+*/
 export class CelautApp {
-free(): void;
-
- constructor(arg0: string, arg1: string);
-
- write_imagedata(arg0: Uint8Array): void;
-
+  free(): void;
+/**
+* @param {string} seed_src 
+* @param {string} table_src 
+* @returns {CelautApp} 
+*/
+  constructor(seed_src: string, table_src: string);
+/**
+* @param {Uint8Array} buffer 
+*/
+  write_imagedata(buffer: Uint8Array): void;
 }
+
+/**
+* If `module_or_path` is {RequestInfo}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {RequestInfo | BufferSource | WebAssembly.Module} module_or_path
+*
+* @returns {Promise<any>}
+*/
+export default function init (module_or_path: RequestInfo | BufferSource | WebAssembly.Module): Promise<any>;
+        
